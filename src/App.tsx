07b7c569f13000734d2cf8c542fa2c17298cc0e9
Levy Chacon <levychacon@yahoo.com>
2021-08-27 13:51:28 -0300

    Styled components adicionado e global css configurado

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import { GlobalStyle } from "./styles/global";
 
 
 
-function App() {
+
+
+
+export function App() {
   return (
     <div className="App">
      <h1>Hello world</h1>
-     
+     <GlobalStyle/>
     </div>
   );
 }
 
-export default App;
